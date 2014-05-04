453d66602faa1fffced83b23710eb2480b888d05
Vitaliy <vpetrov@productengine.com>
2014-05-04 16:24:57 +0300

    Added root panel class

---
+++ Air Viewer/src/com/vp/airviewer/ui/RootPanel.java
@@ -0,0 +1,12 @@
+package com.vp.airviewer.ui;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vitaliy
+ * Date: 5/4/14
+ * Time: 4:22 PM
+ */
+public class RootPanel extends JPanel {
+}
